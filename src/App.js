de7a09ffd9b4f8f8e6644ac78c3b3f250fc7ed45
Mishan Poudel <poudelmishan2@gmail.com>
2024-01-27 19:50:54 +0000

    feat: create end page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import Home from "./components/Home";
 // import Selection from "./components/Selection";
 import GameMode from "./components/GameMode";
+import End from "./components/End";
 
 function App() {
   return (
     <>
       {/* <Home/> */}
       {/* <Selection/> */}
-      <GameMode/>
+      {/* <GameMode/> */}
+      <End/>
     </>
   );
 }
